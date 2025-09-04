8e107b99a80d373db922a5651361bb6348b8c2b2
suywho <suy.nepal@gmail.com>
2025-09-04 15:39:14 +0545

    Update play.html and fonts

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,5 @@ import game.views as views
 
 urlpatterns = [
     path("", views.home_view, name='home'),
-    path("create/", views.create_room, name='create room'),
+    path("play/", views.create_room, name='create room'),
 ]
